58973edad90e62b4b0427d86c2bd25f61bcda94d
simbon4ik <karpov.ivan123@mail.ru>
2025-03-05 14:10:16 +0300

    add check for max summ

--- prog.c
+++ prog.c
@@ -5,21 +5,36 @@
 #define default_len 10
 
 int* foo(int* array, int len, int* new_len){
-    int cnt = 1;
-    int maximum = 0;
-    int index_maximum = 0;
+    int cnt = 1;    
+    int maximum = 0;            //Количество элементов в макс.посл.
+    int index_maximum = 0;      //Индекс, с которого начинается
+    int max_sum = 0;            //Максимальная сумма элементов
+    int temp_sum = array[0];    //Сумма для каждой последовательности
     for (int i = 1; i < len; ++i){
         if (array[i] > array[i-1]){
             cnt += 1;
+            temp_sum += array[i];
         }
         else{
-            maximum = (maximum > cnt) ? maximum : cnt;          //Количество эл. в макс. последовательности
-            index_maximum = (maximum > cnt) ? i : index_maximum;    //Индекс, с которого она начинается
+            if (cnt > maximum){     //Если нашли длиннее
+                maximum = cnt;
+                index_maximum = i;
+                max_sum = temp_sum;
+            }else{
+                if ((cnt == maximum) && (temp_sum > max_sum)){        //Случай, если последовательности равной длины, выбираем с макс.суммой
+                    maximum = cnt;
+                    index_maximum = i;
+                    max_sum = temp_sum;
+                }
+            }
             cnt = 1;
+            temp_sum = array[i];
         }
     }
+    max_sum = (max_sum > temp_sum) ? max_sum : temp_sum;
     printf("max count is %d\n", maximum);
     printf("index max count is %d\n", index_maximum);
+    printf("max sum is %d\n", max_sum);
     int* result = (int*)malloc(maximum*sizeof(int));
     for (int i = 0; i < maximum; ++i){
         result[i] = array[index_maximum];
@@ -34,7 +49,7 @@ int* foo(int* array, int len, int* new_len){
 
 int main(){
     int *array = (int*)malloc(default_len*sizeof(int));
-    int temp[] = {2, 5, 3, 1, 15, 19, 23, 4, 9, 10};
+    int temp[] = {5,7,1,2,3,5,4,10,15,40};      //1,2,3,5 и 4,10,15,40 - две последовательности длины 4, но сумма больше у второй
     memcpy(array, temp, default_len * sizeof(int));
     int new_len = 0;
     int* result = foo(array, default_len, &new_len);
