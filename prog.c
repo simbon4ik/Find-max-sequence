25e9a716811bae99c2ad197a05c4329529912c0d
simbon4ik <karpov.ivan123@mail.ru>
2025-03-05 19:30:25 +0300

    add Makefile, change pos main and foo

--- prog.c
+++ prog.c
@@ -4,6 +4,25 @@
 
 #define default_len 10
 
+int* foo(int* array, int len, int* new_len);
+
+int main(){
+    int *array = (int*)malloc(default_len*sizeof(int));
+    int temp[] = {5,7,2,3,5,6,4,10,15,40};      //2,3,5,6 и 4,10,15,40 - две последовательности длины 4, но сумма больше у второй
+    memcpy(array, temp, default_len * sizeof(int));
+    int new_len = 0;
+    int* result = foo(array, default_len, &new_len);
+    free(array);
+    printf("Result is: ");
+    for(int i = 0; i < new_len; ++i){
+        printf("%d ",result[i]);
+    }
+    printf("\n");
+    free(result);
+    return 0;
+}
+
+
 int* foo(int* array, int len, int* new_len){
     int cnt = 1;    
     int maximum = 0;            //Количество элементов в макс.посл.
@@ -31,32 +50,12 @@ int* foo(int* array, int len, int* new_len){
         temp_sum = array[i];    //Обновление суммы
         temp_ind = i;           //Обновление индекса
     }
-    printf("max count is %d\n", maximum);
-    printf("index max count is %d\n", index_maximum);
-    printf("max sum is %d\n", max_sum);
     int* result = (int*)malloc(maximum*sizeof(int));
     for (int i = 0; i < maximum; ++i){
         result[i] = array[index_maximum];
         index_maximum += 1;
-        printf("result iss %d, index is %d \n", result[i], index_maximum);
     }
     *new_len = maximum;   //Длина результирующего массива
-    printf("here is okay\n");
     return result;
 }
 
-
-int main(){
-    int *array = (int*)malloc(default_len*sizeof(int));
-    int temp[] = {5,7,2,3,5,6,4,10,15,40};      //2,3,5,6 и 4,10,15,40 - две последовательности длины 4, но сумма больше у второй
-    memcpy(array, temp, default_len * sizeof(int));
-    int new_len = 0;
-    int* result = foo(array, default_len, &new_len);
-    free(array);
-    printf("new_len %d\n",new_len);
-    for(int i = 0; i < new_len; ++i){
-        printf("%d\n",result[i]);
-    }
-    free(result);
-    return 0;
-}
