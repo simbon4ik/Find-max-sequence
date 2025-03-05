96d74eff59df0d111b59ab79b386169c2672628b
simbon4ik <karpov.ivan123@mail.ru>
2025-03-05 19:27:15 +0300

    fix bugs in algorithm

--- prog.c
+++ prog.c
@@ -7,6 +7,7 @@
 int* foo(int* array, int len, int* new_len){
     int cnt = 1;    
     int maximum = 0;            //Количество элементов в макс.посл.
+    int temp_ind = 0;           //Временный индекс
     int index_maximum = 0;      //Индекс, с которого начинается
     int max_sum = 0;            //Максимальная сумма элементов
     int temp_sum = array[0];    //Сумма для каждой последовательности
@@ -14,24 +15,22 @@ int* foo(int* array, int len, int* new_len){
         if (array[i] > array[i-1]){
             cnt += 1;
             temp_sum += array[i];
+            if (i != len-1) continue;     //Если последний элемент, чтобы обновить max_sum и все остальное
         }
-        else{
-            if (cnt > maximum){     //Если нашли длиннее
-                maximum = cnt;
-                index_maximum = i;
-                max_sum = temp_sum;
-            }else{
-                if ((cnt == maximum) && (temp_sum > max_sum)){        //Случай, если последовательности равной длины, выбираем с макс.суммой
-                    maximum = cnt;
-                    index_maximum = i;
-                    max_sum = temp_sum;
-                }
-            }
-            cnt = 1;
-            temp_sum = array[i];
+        if (cnt > maximum){     //Если нашли длиннее
+            maximum = cnt;
+            index_maximum = temp_ind;
+            max_sum = temp_sum;
         }
+        if ((cnt == maximum) && (temp_sum > max_sum)){        //Случай, если последовательности равной длины, выбираем с макс.суммой
+            maximum = cnt;
+            index_maximum = temp_ind;
+            max_sum = temp_sum;
+        }
+        cnt = 1;                //Обновление счетчика длины
+        temp_sum = array[i];    //Обновление суммы
+        temp_ind = i;           //Обновление индекса
     }
-    max_sum = (max_sum > temp_sum) ? max_sum : temp_sum;
     printf("max count is %d\n", maximum);
     printf("index max count is %d\n", index_maximum);
     printf("max sum is %d\n", max_sum);
@@ -49,7 +48,7 @@ int* foo(int* array, int len, int* new_len){
 
 int main(){
     int *array = (int*)malloc(default_len*sizeof(int));
-    int temp[] = {5,7,1,2,3,5,4,10,15,40};      //1,2,3,5 и 4,10,15,40 - две последовательности длины 4, но сумма больше у второй
+    int temp[] = {5,7,2,3,5,6,4,10,15,40};      //2,3,5,6 и 4,10,15,40 - две последовательности длины 4, но сумма больше у второй
     memcpy(array, temp, default_len * sizeof(int));
     int new_len = 0;
     int* result = foo(array, default_len, &new_len);
